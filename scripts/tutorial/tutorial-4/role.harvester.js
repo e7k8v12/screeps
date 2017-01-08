5eaf21850baf7ebca3d9b246a817a2b6c239e0df
Eugene <e7k8v12@gmail.com>
2017-01-08 11:02:19 +0800

    with tutorials

--- scripts/tutorial/tutorial-4/role.harvester.js
+++ scripts/tutorial/tutorial-4/role.harvester.js
@@ -1,27 +1,28 @@
 var roleHarvester = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+    run: function (creep) {
+        if (creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
+                        filter: (structure) = > {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                structure.energy < structure.energyCapacity;
+        }
+        })
+            ;
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
         }
-	}
+    }
 };
 
 module.exports = roleHarvester;
